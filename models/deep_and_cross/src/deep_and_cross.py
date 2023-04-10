00fdcaaa60f63b0dcd99494a3f8491f839e9e9d1
lizhenyu <lizhenyu13@huawei.com>
2023-04-10 11:07:55 +0800

    Remove the use of Dropout's deprecated parameter

--- models/deep_and_cross/src/deep_and_cross.py
+++ models/deep_and_cross/src/deep_and_cross.py
@@ -15,35 +15,31 @@
 """deep and cross net"""
 
 
-import numpy as np
-from mindspore import nn
-from mindspore import ParameterTuple
 import mindspore.common.dtype as mstype
-from mindspore.ops import functional as F
-from mindspore.ops import composite as C
-from mindspore.ops import operations as P
+import numpy as np
+from mindspore import ParameterTuple, Tensor, nn
+from mindspore.common.initializer import Uniform, initializer
+from mindspore.common.parameter import Parameter
 from mindspore.nn import Dropout
 from mindspore.nn.optim import Adam
-from mindspore.common.initializer import Uniform, initializer
-from mindspore import Tensor
 from mindspore.ops import Squeeze
-from mindspore.common.parameter import Parameter
-
+from mindspore.ops import composite as C
+from mindspore.ops import functional as F
+from mindspore.ops import operations as P
 
 ms_type = mstype.float32
 ds_type = mstype.int32
 
 
 def init_method(method, shape, name, max_val=1.0):
-    '''
+    """
     parameter init method
-    '''
-    if method in ['uniform']:
-        params = Parameter(initializer(
-            Uniform(max_val), shape, ms_type), name=name)
+    """
+    if method in ["uniform"]:
+        params = Parameter(initializer(Uniform(max_val), shape, ms_type), name=name)
     elif method == "one":
         params = Parameter(initializer("ones", shape, ms_type), name=name)
-    elif method == 'zero':
+    elif method == "zero":
         params = Parameter(initializer("zeros", shape, ms_type), name=name)
     elif method == "normal":
         params = Parameter(initializer("normal", shape, ms_type), name=name)
@@ -55,25 +51,32 @@ def normal_weight(shape, num_units):
     return Tensor(norm)
 
 
-
 class DenseLayer(nn.Cell):
     """
     Dense Layer for DCN Model;
     Containing: activation, matmul, bias_add;
     """
 
-    def __init__(self, input_dim, output_dim, weight_bias_init, act_str,
-                 keep_prob=0.5, use_activation=True, convert_dtype=True, drop_out=False):
-        super(DenseLayer, self).__init__()
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        weight_bias_init,
+        act_str,
+        keep_prob=0.5,
+        use_activation=True,
+        convert_dtype=True,
+        drop_out=False,
+    ):
+        super().__init__()
         weight_init, bias_init = weight_bias_init
-        self.weight = init_method(
-            weight_init, [input_dim, output_dim], name="weight")
+        self.weight = init_method(weight_init, [input_dim, output_dim], name="weight")
         self.bias = init_method(bias_init, [output_dim], name="bias")
         self.act_func = self._init_activation(act_str)
         self.matmul = P.MatMul(transpose_b=False)
         self.bias_add = P.BiasAdd()
         self.cast = P.Cast()
-        self.dropout = Dropout(keep_prob=keep_prob)
+        self.dropout = Dropout(p=1 - keep_prob)
         self.use_activation = use_activation
         self.convert_dtype = convert_dtype
         self.drop_out = drop_out
@@ -89,9 +92,9 @@ class DenseLayer(nn.Cell):
         return act_func
 
     def construct(self, x):
-        '''
+        """
         Construct Dense layer
-        '''
+        """
         if self.training and self.drop_out:
             x = self.dropout(x)
         if self.convert_dtype:
@@ -115,8 +118,12 @@ class CrossLayer(nn.Cell):
     """
     Cross Layer for  DCN Model;
     """
-    def __init__(self, cross_raw_dim, cross_col_dim, weight_bias_init, convert_dtype=True):
-        super(CrossLayer, self).__init__()
+
+    # pylint: disable=W0613
+    def __init__(
+        self, cross_raw_dim, cross_col_dim, weight_bias_init, convert_dtype=True
+    ):
+        super().__init__()
         weight_init, bias_init = weight_bias_init
         self.cross_weight = init_method(weight_init, [cross_col_dim, 1], name="weight")
         self.cross_bias = init_method(bias_init, [cross_col_dim, 1], name="bias")
@@ -130,9 +137,9 @@ class CrossLayer(nn.Cell):
         self.squeeze = Squeeze(2)
 
     def construct(self, inputs, x_0):
-        '''
+        """
         Construct Cross layer
-        '''
+        """
         x_0 = self.expand_dims(x_0, 2)
         x_l = self.expand_dims(inputs, 2)
         x_lw = C.tensor_dot(x_l, self.cross_weight, ((1,), (0,)))
@@ -152,16 +159,22 @@ class EmbeddingLookup(nn.Cell):
         use_one_hot_embeddings (bool): Specifies whether to use one hot encoding form. Default: False.
         initializer_range (float): Initialization value of TruncatedNormal. Default: 0.02.
     """
-    def __init__(self,
-                 vocab_size,
-                 embedding_size,
-                 use_one_hot_embeddings=False,
-                 initializer_range=0.02):
-        super(EmbeddingLookup, self).__init__()
+
+    # pylint: disable=W0613
+    def __init__(
+        self,
+        vocab_size,
+        embedding_size,
+        use_one_hot_embeddings=False,
+        initializer_range=0.02,
+    ):
+        super().__init__()
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.use_one_hot_embeddings = use_one_hot_embeddings
-        self.embedding_table = Parameter(normal_weight([vocab_size, embedding_size], embedding_size))
+        self.embedding_table = Parameter(
+            normal_weight([vocab_size, embedding_size], embedding_size)
+        )
         self.expand = P.ExpandDims()
         self.shape_flat = (-1,)
         self.gather = P.Gather()
@@ -178,7 +191,9 @@ class EmbeddingLookup(nn.Cell):
 
         flat_ids = self.reshape(input_ids, self.shape_flat)
         if self.use_one_hot_embeddings:
-            one_hot_ids = self.one_hot(flat_ids, self.vocab_size, self.on_value, self.off_value)
+            one_hot_ids = self.one_hot(
+                flat_ids, self.vocab_size, self.on_value, self.off_value
+            )
             output_for_reshape = self.array_mul(one_hot_ids, self.embedding_table)
         else:
             output_for_reshape = self.gather(self.embedding_table, flat_ids, 0)
@@ -192,8 +207,9 @@ class DeepCrossModel(nn.Cell):
     """
     Deep and Cross Model
     """
+
     def __init__(self, config):
-        super(DeepCrossModel, self).__init__()
+        super().__init__()
         self.concat = P.Concat(axis=1)
         self.reshape = P.Reshape()
         self.deep_reshape = P.Reshape()
@@ -208,29 +224,71 @@ class DeepCrossModel(nn.Cell):
         self.cross_layer_num = config.cross_layer_num
         self.keep_prob = config.keep_prob
 
-        self.cross_layer_1 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
-        self.cross_layer_2 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
-        self.cross_layer_3 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
-        self.cross_layer_4 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
-        self.cross_layer_5 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
-        self.cross_layer_6 = CrossLayer(self.batch_size,
-                                        self.input_size, weight_bias_init=['normal', 'normal'], convert_dtype=False)
+        self.cross_layer_1 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
+        self.cross_layer_2 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
+        self.cross_layer_3 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
+        self.cross_layer_4 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
+        self.cross_layer_5 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
+        self.cross_layer_6 = CrossLayer(
+            self.batch_size,
+            self.input_size,
+            weight_bias_init=["normal", "normal"],
+            convert_dtype=False,
+        )
 
-        self.dense_layer_1 = DenseLayer(self.input_size, self.deep_layer_dim[0],
-                                        weight_bias_init=['normal', 'normal'], act_str="relu",
-                                        keep_prob=self.keep_prob, convert_dtype=False, drop_out=False)
-        self.dense_layer_2 = DenseLayer(self.deep_layer_dim[0], self.deep_layer_dim[1],
-                                        weight_bias_init=['normal', 'normal'], act_str="relu",
-                                        keep_prob=self.keep_prob, convert_dtype=False, drop_out=False)
-        self.dense_layer_3 = DenseLayer(self.input_size + self.deep_layer_dim[1],
-                                        1, weight_bias_init=['normal', 'normal'], act_str="sigmoid",
-                                        keep_prob=self.keep_prob, use_activation=False,
-                                        convert_dtype=False, drop_out=False)
+        self.dense_layer_1 = DenseLayer(
+            self.input_size,
+            self.deep_layer_dim[0],
+            weight_bias_init=["normal", "normal"],
+            act_str="relu",
+            keep_prob=self.keep_prob,
+            convert_dtype=False,
+            drop_out=False,
+        )
+        self.dense_layer_2 = DenseLayer(
+            self.deep_layer_dim[0],
+            self.deep_layer_dim[1],
+            weight_bias_init=["normal", "normal"],
+            act_str="relu",
+            keep_prob=self.keep_prob,
+            convert_dtype=False,
+            drop_out=False,
+        )
+        self.dense_layer_3 = DenseLayer(
+            self.input_size + self.deep_layer_dim[1],
+            1,
+            weight_bias_init=["normal", "normal"],
+            act_str="sigmoid",
+            keep_prob=self.keep_prob,
+            use_activation=False,
+            convert_dtype=False,
+            drop_out=False,
+        )
 
     def construct(self, id_hldr, wt_hldr):
         """dcn construct"""
@@ -255,16 +313,17 @@ class NetWithLossClass(nn.Cell):
     """
     NetWithLossClass definition.
     """
+
     def __init__(self, network):
-        super(NetWithLossClass, self).__init__(auto_prefix=False)
+        super().__init__(auto_prefix=False)
         self.loss = P.SigmoidCrossEntropyWithLogits()
         self.network = network
-        self.ReduceMean = P.ReduceMean(keep_dims=False)
+        self.reduce_mean = P.ReduceMean(keep_dims=False)
 
     def construct(self, batch_ids, batch_wts, label):
         predict = self.network(batch_ids, batch_wts)
         log_loss = self.loss(predict, label)
-        mean_log_loss = self.ReduceMean(log_loss)
+        mean_log_loss = self.reduce_mean(log_loss)
         loss = mean_log_loss
         return loss
 
@@ -273,13 +332,16 @@ class TrainStepWrap(nn.Cell):
     """
     TrainStepWrap definition
     """
+
     def __init__(self, network, lr=0.0001, eps=1e-8, loss_scale=1000.0):
-        super(TrainStepWrap, self).__init__(auto_prefix=False)
+        super().__init__(auto_prefix=False)
         self.network = network
         self.network.set_grad()
         self.network.set_train()
         self.weights = ParameterTuple(network.trainable_params())
-        self.optimizer = Adam(self.weights, learning_rate=lr, eps=eps, loss_scale=loss_scale)
+        self.optimizer = Adam(
+            self.weights, learning_rate=lr, eps=eps, loss_scale=loss_scale
+        )
         self.hyper_map = C.HyperMap()
         self.grad = C.GradOperation(get_by_list=True, sens_param=True)
         self.sens = loss_scale
@@ -291,12 +353,14 @@ class TrainStepWrap(nn.Cell):
         grads = self.grad(self.network, weights)(batch_ids, batch_wts, label, sens)
         return F.depend(loss, self.optimizer(grads))
 
+
 class PredictWithSigmoid(nn.Cell):
     """
     Eval model with sigmoid.
     """
+
     def __init__(self, network):
-        super(PredictWithSigmoid, self).__init__(auto_prefix=False)
+        super().__init__(auto_prefix=False)
         self.network = network
         self.sigmoid = P.Sigmoid()
 
